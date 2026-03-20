d21b29bbbf80ffd135d66b72b4e20a7e63484689
quoclongnguyen2003 <quoclongnguyen2003@gmail.com>
2026-03-20 12:09:37 +0700

    add category && product models

---
+++ backend/src/models/productModel.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const productSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "Category",
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    images: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
