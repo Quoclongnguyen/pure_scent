51dd5d5c60dfa191f4ef69a6ab01a205e4d5bd1d
quoclongnguyen2003 <quoclongnguyen2003@gmail.com>
2026-03-26 14:58:25 +0700

    feat: add product filter by gender on HomePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ProductCard from '../components/productCard/ProductCard'
+const mockProducts = [
+    { id: 1, name: "Santal 33", brand: "Le Labo", gender: "Unisex", price: "5.500.000đ", image: "/src/assets/img1.png" },
+    { id: 2, name: "Another 13", brand: "Le Labo", gender: "Men", price: "5.200.000đ", image: "/src/assets/img1.png" },
+    { id: 3, name: "Rose 31", brand: "Le Labo", gender: "Women", price: "4.800.000đ", image: "/src/assets/img1.png" },
+    { id: 5, name: "Lys 41", brand: "Le Labo", gender: "Women", price: "5.000.000đ", image: "/src/assets/img1.png" },
+    { id: 6, name: "Lys 41", brand: "Le Labo", gender: "Men", price: "5.000.000đ", image: "/src/assets/img1.png" },
+    { id: 7, name: "Lys 41", brand: "Le Labo", gender: "Women", price: "5.000.000đ", image: "/src/assets/img1.png" },
+    { id: 8, name: "Lys 41", brand: "Le Labo", gender: "Men", price: "5.000.000đ", image: "/src/assets/img1.png" },
+    { id: 9, name: "Lys 41", brand: "Le Labo", gender: "Men", price: "5.000.000đ", image: "/src/assets/img1.png" },
 
+];
 const HomePage = () => {
+    const [filter, setFilter] = useState("All")
+
+    const filterredProducts = filter === "All" ? mockProducts : mockProducts.filter(p => p.gender === filter)
+
     return (
         <>
             <main className='min-h-screen bg-white'>
@@ -21,7 +36,7 @@ const HomePage = () => {
                                     Chào mừng bạn đến với thế giới nước hoa tinh tế, nơi mỗi mùi hương
                                     là một câu chuyện kể về sự sang trọng và phong cách cá nhân.
                                 </p>
-                                <button className='bg-gray-500 text-white  p-4 uppercase text-xs tracking-widest hover:bg-gray-800 transition-all font-medium'>
+                                <button className='bg-gray-500 text-white  p-4 uppercase text-xs tracking-widest hover:bg-gray-800 transition-all font-medium cursor-pointer'>
                                     Mua Ngay
                                 </button>
                             </div>
@@ -38,8 +53,40 @@ const HomePage = () => {
                     </div>
 
 
+
                 </section>
-            </main>
+
+                <section className='max-w-7xl mx-auto px-10 py-20'>
+
+                    <h2 className='font-serif text-3xl text-center mb-16 uppercase tracking-widest'>
+                        Sản phẩm mới nhất
+                    </h2>
+                    <div className='flex justify-center gap-10 mb-16 text-xs uppercase tracking-[0.2rem] font-medium'>
+                        {['All', 'Men', 'Women', 'Unisex'].map((cat) =>
+                            <button
+                                key={cat}
+                                onClick={() => setFilter(cat)}
+                                className={`cursor-pointer transition-all ${filter === cat
+                                    ? 'text-black border-b border-black pb-1'
+                                    : 'text-gray-400 hover:text-black'} `}
+                            >
+                                {cat === 'All' ? 'Tất cả' : cat}
+                            </button>
+                        )}
+                    </div>
+
+
+                    {/* Grid chia 4 cột trên máy tính, 2 cột trên điện thoại */}
+                    <div className='grid grid-cols-2 lg:grid-cols-4 gap-10'>
+                        {filterredProducts.map((item) => (
+                            <ProductCard key={item.id} product={item} />
+                        ))}
+
+                    </div>
+
+                </section>
+
+            </main >
 
         </>
 
