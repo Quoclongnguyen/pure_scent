11753758592d35a16e4c537e840ba86e36cba3e4
quoclongnguyen2003 <quoclongnguyen2003@gmail.com>
2026-05-05 10:32:58 +0700

    feat: extend user model with roles (user, staff, superAdmin)

--- backend/src/models/userModel.js
+++ backend/src/models/userModel.js
@@ -21,6 +21,11 @@ const userSchema = mongoose.Schema(
       required: true,
       default: false,
     },
+    role: {
+      type: String,
+      enum: ['user', 'staff', 'superAdmin'],
+      default: 'user',
+    },
   },
   {
     timestamps: true,
